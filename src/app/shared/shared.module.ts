0a5d53fda682107604d137f1a2f218d2a164baf3
Leandro Bragante <bragante.leandro@outlook.com>
2024-01-29 12:06:27 -0300

    fix: remove componentes navbar e footer para a pasta core

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from '../core/components/navbar/navbar.component';
 import { UserRoutingModule } from '../pages/users/user-routing.module';
 import { UserCardComponent } from './components/user-card/user-card.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { AlertComponent } from './components/alert/alert.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from '../core/components/footer/footer.component';
 
 
 @NgModule({
